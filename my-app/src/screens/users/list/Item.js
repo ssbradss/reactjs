0cbdd5bda337f1111e3dc3c11059bd07e1e874f5
baonq <baonq@deha-soft.com>
2020-09-28 15:36:10 +0700

    update

--- my-app/src/screens/users/list/Item.js
+++ my-app/src/screens/users/list/Item.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {all, remove} from "../../../services/book";
+import {Link, Route, Switch, useRouteMatch} from "react-router-dom";
 
 const onDelete = async (user) => {
   const confirm = window.confirm(`Do you want to delete ${user.UserName}`);
@@ -10,12 +11,14 @@ const onDelete = async (user) => {
 };
 
 export default function Item({user}) {
+  let { path, url } = useRouteMatch();
   return (
     <tr>
       <td>{user.ID}</td>
       <td>{user.UserName}</td>
       <td>{user.Password}</td>
-      <td><button onClick={() => onDelete(user)}>Del</button></td>
+      <td><button onClick={() => onDelete(user)}>Del</button>
+      <Link to={`${url}/` + user.ID}>Edit</Link></td>
     </tr>
   );
 }
